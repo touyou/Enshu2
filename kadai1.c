97e4878dcd234623af1cf44d44e7d974e4a05760
touyou <fujiyou1121@gmail.com>
2017-09-29 15:06:19 +0900

    test clear

--- kadai1.c
+++ kadai1.c
@@ -1,5 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char **argv) {
     int myid, numproc;
@@ -8,9 +10,14 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &numproc);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-    printf("process [%d]\n", myid);
-    if (myid == 0) {
-        printf("# of process [%d]\n", numproc);
+    if(myid == 0) {
+        int n = argc[0];
+        int p = numproc;
+        srand((unsigned)time(NULL));
+        int nums[n];
+        for (int i=0; i<n; i++) nums[i] = rand();
+
+    } else {
     }
 
     MPI_Finalize();
