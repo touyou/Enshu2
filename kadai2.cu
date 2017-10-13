d7b3cb38a893656cd2952b2346ea1f3a35fd7e34
touyou <fujiyou1121@gmail.com>
2017-10-13 15:08:02 +0900

    cpu done

--- kadai2.cu
+++ kadai2.cu
@@ -5,7 +5,7 @@
 #include <cuda_runtime.h>
 #include <omp.h>
 
-#define TIMEMAX 30
+#define TIMEMAX 100
 #define XSIZE 50
 #define YSIZE 50
 
@@ -18,10 +18,11 @@
     }
 }*/
 
+// blockIdx, blockDim, threadIdx, gridDim
 __global__ void simmGpu(float ***u, float r) {
 }
 
-void simmCpu(float (*u)[2][XSIZE][YSIZE], float r) {
+void simmCpu(float u[2][XSIZE][YSIZE], float r) {
     int t, i, j;
     omp_set_num_threads(8);
 
@@ -30,7 +31,7 @@ void simmCpu(float (*u)[2][XSIZE][YSIZE], float r) {
         for (i=1; i<XSIZE-1; i++) {
             #pragma omp parallel for
             for (j=1; j<YSIZE-1; j++) {
-                (*u)[(t+1)%2][i][j] = (1.0 - 4.0*r) * (*u)[t%2][i][j] + r * ((*u)[t%2][i+1][j] + (*u)[t%2][i-1][j] + (*u)[t%2][i][j+1] + (*u)[t%2][i][j-1]);
+                u[(t+1)%2][i][j] = (1.0 - 4.0*r) * u[t%2][i][j] + r * (u[t%2][i+1][j] + u[t%2][i-1][j] + u[t%2][i][j+1] + u[t%2][i][j-1]);
             }
         }
     }
@@ -41,25 +42,35 @@ int main() {
     float ***devA;
     float u[2][XSIZE][YSIZE];
     int nb = 2 * XSIZE * YSIZE * sizeof(float), i, j;
+    memset(u, 0, nb);
     for (i=1; i<XSIZE-1; i++) {
-        for (j=1; j<YSIZE; j++) {
+        for (j=1; j<YSIZE-1; j++) {
             u[0][i][j] = 1;
         }
     }
 
     gettimeofday(&t0, NULL);
-    simmCpu(&u, 0.12);
-    /*cudaMalloc((void****)&devA, nb);
-    cudaMemcpy(devA, a, nb, cudaMemcpyHostToDevice);
-    add<<<2, 3>>>(devA, devA, 8);
-    cudaMemcpy(a, devA, nb, cudaMemcpyDeviceToHost);
-    cudaThreadSynchronize();*/
+    // simmCpu(u, 0.12);
+    cudaMalloc((void****)&devA, nb);
+    cudaMemcpy(devA, u, nb, cudaMemcpyHostToDevice);
+    simmGpu<<<2, 3>>>(devA, 8);
+    cudaMemcpy(u, devA, nb, cudaMemcpyDeviceToHost);
+    cudaThreadSynchronize();
     gettimeofday(&t1, NULL);
     printf("Elapsed time = %lf\n", (double)(t1.tv_sec-t0.tv_sec)+(double)(t1.tv_usec-t0.tv_usec)*1.0e-6);
+
     for (i=0; i<XSIZE; i++) {
         for (j=0; j<YSIZE; j++) {
-            printf("%.2lf ", u[0][i][j]);
+            if (u[0][i][j] > 0.34) printf("#");
+            else if (u[0][i][j] > -0.34) printf("*");
+            else printf(".");
         }
         puts("");
     }
+    /*for (i=0; i<XSIZE; i++) {
+        for (j=0; j<YSIZE; j++) {
+            printf("%+02.2lf ", u[0][i][j]);
+        }
+        puts("");
+    }*/
 }
